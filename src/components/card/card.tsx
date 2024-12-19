7aafbf15875bfc164482d770e3ff0195f23990a3
NilufarSalimova2000 <nilufarsalimova2000@gmail.com>
2024-12-19 13:07:48 +0500

    add

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,29 +1,30 @@
 import Image from "next/image";
 
 interface Type {
-    img: string;
+  img: string;
   title: string;
   rame: string;
   color: string;
   brand: string;
   price: number;
   id: number;
-  }
+}
 
 export const Card = ({ item }: { item: Type }) => {
-    return (
-        <div style={{
-            border: "1px solid #ddd",
-            borderRadius: "8px",
-            padding: "16px",
-            width: "250px"
-          }}>
-            <Image src={item.img} alt={item.title} style={{ width: "100%", height: "200px", objectFit: "cover" }} />
-            <h3>{item.title}</h3>
-            <p><strong>RAM/Storage:</strong> {item.rame}</p>
-            <p><strong>Color:</strong> {item.color}</p>
-            <p><strong>Brand:</strong> {item.brand}</p>
-            <p><strong>Price:</strong> {item.price.toLocaleString()} so'm</p>
-          </div>
-    );
-  };
+  return (
+    <div style={{
+      border: "1px solid #ddd",
+      borderRadius: "8px",
+      padding: "16px",
+      width: "250px"
+    }}>
+      <Image src={item.img} alt={item.title} width={250} // Rasm kengligi
+        height={150} style={{ objectFit: "cover" }} />
+      <h3>{item.title}</h3>
+      <p><strong>RAM/Storage:</strong> {item.rame}</p>
+      <p><strong>Color:</strong> {item.color}</p>
+      <p><strong>Brand:</strong> {item.brand}</p>
+      <p><strong>Price:</strong> {item.price.toLocaleString()} so'm</p>
+    </div>
+  );
+};
