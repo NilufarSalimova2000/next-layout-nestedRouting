609ec9846f6f601409af7c78deeba586cba39cad
NilufarSalimova2000 <nilufarsalimova2000@gmail.com>
2024-12-19 13:16:43 +0500

    metadata add

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    images: {
+      domains: ["raw.githubusercontent.com"], // Rasmlar yuklanadigan xostni qo'shish
+    },
   /* config options here */
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ import { Card } from "@/components/card";
 import Link from "next/link";
 import React from "react";
 
+export const metadata = {
+  title: "Phones - Home", // Sahifa sarlavhasi
+  description: "Explore the latest phones available in our store", // Tavsif
+};
+
 export interface Type {
   img: string;
   title: string;
@@ -13,14 +18,14 @@ export interface Type {
 }
 
 const Home = async () => {
-  const data = await fetch("http://localhost:5000/phones");
+  const data = await fetch("http://localhost:5000/phones", { cache: "no-store" });
   const res = await data.json();
 
   return (
     <>
       <div className="py-[50px]">
         <div className="container">
-          <div className="flex  flex-wrap gap-[25px] justify-between">
+          <div className="flex flex-wrap gap-[25px] justify-between">
             {res?.map((item: Type) => (
               <div key={item.id}>
                 <Link href={`/details/${item.id}`}>
